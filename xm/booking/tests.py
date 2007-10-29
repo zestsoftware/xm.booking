a0221964daff251973d225d551bce0b98039c362
Maurits van Rees <maurits@vanrees.org>
2007-10-29 22:32:32 +0000

    We are not using anything from PloneTestCase, so remove that. This speeds up the tests rather heavily as now they are real unit tests. :)
    svn path=/xm.booking/trunk/; revision=52655

--- xm/booking/tests.py
+++ xm/booking/tests.py
@@ -4,30 +4,8 @@ from zope.testing import doctest
 from zope.testing import doctestunit
 from zope.component import testing
 
-from Products.Five import zcml
-from Products.Five import fiveconfigure
-from Products.PloneTestCase import PloneTestCase as ptc
-from Products.PloneTestCase.layer import onsetup
-
 OPTIONFLAGS = (doctest.ELLIPSIS |
                doctest.NORMALIZE_WHITESPACE)
-
-@onsetup
-def xm_setup():
-    """Set up our Plone Site.
-    """
-    fiveconfigure.debug_mode = True
-    import xm.booking
-    zcml.load_config('configure.zcml', xm.booking)
-    fiveconfigure.debug_mode = False
-
-xm_setup()
-ptc.setupPloneSite()
-
-class TestCase(ptc.PloneTestCase):
-    """Base test case.
-    """
-    pass
     
 
 def test_suite():
