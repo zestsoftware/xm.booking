fc15b077420367dd8c21e0169dcb94e04a85f4f1
Maurits van Rees <maurits@vanrees.org>
2008-07-31 16:48:49 +0000

    Fixed UnAuthorized error when adding a booking via KSS.  Real sneaky.
    svn path=/xm.booking/branches/0.5/; revision=69306

--- xm/booking/browser/viewlets.py
+++ xm/booking/browser/viewlets.py
@@ -3,6 +3,17 @@ from plone.app.layout.viewlets import ViewletBase
 from Products.CMFCore.utils import getToolByName
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
+from zope.interface import Interface
+from zope.interface import implements
+
+
+class BookingFormInterface(Interface):
+
+    def allowed():
+        """Is the user allowed to add a booking here?
+        """
+        pass
+
 
 class Bookings(ViewletBase):
 
@@ -10,8 +21,11 @@ class Bookings(ViewletBase):
 
 
 class BookingForm(ViewletBase):
+    implements(BookingFormInterface)
 
     def allowed(self):
+        """Is the user allowed to add a booking here?
+        """
         context = aq_inner(self.context)
         mem = getToolByName(context, 'portal_membership')
         return mem.checkPermission('eXtremeManagement: Add Booking', context)
